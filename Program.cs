0fa92c1584c9b1c21e1eb7901204ba018ad3862a
Sophia X Cui <sophiaxcui@gmail.com>
2011-06-27 16:46:15 -0700

    improvements

--- Program.cs
+++ Program.cs
@@ -10,6 +10,63 @@ namespace primenumbergenerator
     {
         static void Main(string[] args)
         {
+            bool slow = false;
+            char fastOrSlow = 'F';
+
+            Console.Write("(S)low or (F)ast?");
+            string speed = Console.ReadLine();
+            if (Char.TryParse(speed, out fastOrSlow))
+            {
+                if (fastOrSlow.Equals('S'))
+                {
+                    slow = true;
+                }
+            }
+
+            if (slow)
+                slowPrimeNumbers();
+            else
+                fastPrimeNumbers();
+        }
+
+        /// <summary>
+        /// improved version, probably O(n*log(n))
+        /// </summary>
+        private static void fastPrimeNumbers()
+        {
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+
+            int index = 3;
+            while (true)
+            {
+                bool isPrime = true;
+
+                for (int i = 0; i < primeNumbers.Count; i++)
+                {
+                    if ((index % primeNumbers[i]) == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.Write(index + " ");
+                    primeNumbers.Add(index);
+                    Thread.Sleep(500);
+                }
+
+                index++;
+            }
+        }
+
+        /// <summary>
+        /// basic prime number generator O(n^2)
+        /// </summary>
+        private static void slowPrimeNumbers()
+        {
             int index = 1;
             while (true)
             {
