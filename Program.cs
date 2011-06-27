6eb4037c47f9580366e3614582bea54f7e7aa542
Sophia X Cui <sophiaxcui@gmail.com>
2011-06-27 16:25:15 -0700

    source for project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace primenumbergenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (true)
+            {
+                bool isPrime = true;
+
+                for (int i = 2; i < index; i++)
+                {
+                    if ((index % i) == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.Write(index + " ");
+                    Thread.Sleep(500);
+                }
+
+                index++;
+            }
+        }
+    }
+}
